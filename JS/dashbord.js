35911bcda3ef8471bfab70bac528323076c83e58
MariamWand <mariamwandaogo7@gmail.com>
2023-03-20 17:01:46 +0000

    lundi

--- JS/dashbord.js
+++ JS/dashbord.js
@@ -9,37 +9,87 @@
 const dashbord = document.querySelector('.dashbord')
 users = JSON.parse(localStorage.getItem('users'));
 console.log(dashbord);
-let table = document.querySelector('dashbord')
 let tbody = document.createElement('tbody');
 
-function ajoutTD(){
-    for(let i = 0 ; i< users.length ; i++){
-        console.log(users[i])
-        for(let j = 0 ; j < Object.keys(users[i]).length;j++){
-            console.log(Object.values(users[i])[j]);
+// function ajoutTD(){
+//     for(let i = 0 ; i< users.length ; i++){
+//         console.log(users[i])
+//         for(let j = 0 ; j < Object.keys(users[i]).length;j++){
+//             console.log(Object.values(users[i])[0]);
 
 
-            let TD = document.createElement('TD'); 
-            let context = document.createTextNode(Object.values(users[i])[j]);
-            console.log(context);
-        }
-    }
+//             let TD = document.createElement('TD'); 
+//             let context = document.createTextNode(Object.values(users[i])[j]);
+//             console.log(context);
+//         }
+//     }
    
+// }
+// ajoutTD();
+
+// console.log(users);
+// users.forEach(element => {
+//     console.log(element.nom);
+//     console.log(element.prenom);
+//     console.log(element.email);
+//     console.log(element.genre);
+//     console.log(element.date);
+//     console.log(element.heure);
+//     row = `<tr>
+//     <td>${element.nom}</td>
+//     <td>${element.prenom}</td>
+//     <td>${element.email}</td>
+//     <td>${element.genre}</td>
+//     <td>${element.date}</td>
+//     <td>${element.heure}</td>
+//      </tr>`
+
+           
+// });
+
+// table.appendChild(tbody)
+
+// let contacts = getContacts();
+
+function createTable() {
+  for (let index = 0; index < users.length; index++) {
+    let row = document.createElement("tr");
+    // creer le button de supression
+    let buttonCell = document.createElement("td");
+    let deleteButton = document.createElement("button");
+    let buttonText = document.createTextNode("terminer");
+    // deleteButton.setAttribute("class", "delete-btn");
+    deleteButton.appendChild(buttonText);
+
+    for (
+      let element = 0;
+      element < Object.keys(users[0]).length;
+      element++
+    ) {
+        
+        console.log(element)
+
+      // ajouter les td
+      const cell = document.createElement("td");
+
+      const cellText = document.createTextNode( Object.values(users[index])[element]
+      );
+      console.log(cellText);
+      console.log(cell);
+      deleteButton.setAttribute("contactPhone", users[index].telephone);
+      deleteButton.setAttribute("contactPhone", users[index].telephone);
+      buttonCell.appendChild(deleteButton);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+      row.appendChild(buttonCell);
+      row.setAttribute("id", users[index].telephone);
+
+      // console.log( Object.keys(contacts[0]));
+    }
+    console.log('tets',row)
+    tbody.appendChild(row);
+  }
+  dashbord.appendChild(tbody);
 }
-ajoutTD();
 
-for(let keys in users){
-if(keys == "nom"){
-
-}
-  let ligne = document.createElement('th');
-  let nom = document.createElement('td');
-//   nom.textContent = users[];
-
-}
-
-// console.log(tbody);
-// console.log(users)
-// console.log(Object.values(users[0]))
-// console.log(Object.keys(users[0]))
-
+createTable();
--- JS/formulaire.js
+++ JS/formulaire.js
@@ -5,6 +5,7 @@
     let genre = document.querySelector('#genre');
     let date = document.querySelector('#date');
     let heure = document.querySelector('#time');
+    let tache = document.querySelector('#tache');
 
     function recuper(){
         recu = localStorage.getItem('users')
@@ -16,74 +17,35 @@
     }
 
     function ajout(conatct){
-            // let recupere = recuper();
-            let recupere =  [];
+            let recupere = recuper();
             recupere.push(conatct)
             localStorage.setItem('users',JSON.stringify(recupere))
     }
     const btn = document.querySelector('#valider')
     btn.addEventListener('click',function(){
-        const infos = {
-            nom:nom.value,
-            prenom:prenom.value,
-            email:email.value,
-            genre:genre.value,
-            date:date.value,
-            heure:heure.value 
+        if(nom.value === '' || prenom.value === ''  || email.value === '' ||  genre.value === ''||  date.value === '' || heure.value === '' || tache.value === ''){
+            alert('Veuillez Rempli Tout Les Champs Vides')
+        }else{
+            const infos = {
+                nom:nom.value,
+                prenom:prenom.value,
+                email:email.value,
+                genre:genre.value,
+                date:date.value,
+                heure:heure.value,
+                tache:tache.value,
+            }
+
+
+            ajout(infos)
         }
           
         
-          ajout(infos)
+         
 
-          document.location.href="../HTML/dashboard.html";
+            document.location.reload()
     
 
     });
 
-//    function formulaire (){ 
-//         let userN = localStorage.setItem("nom");
-//         let userP = localStorage.setItem("prenom");
-//         let userM = localStorage.setItem("Email");
-//         let userG = localStorage.setItem("genre");
-//         let userD = localStorage.setItem("date");
-//         let userH = localStorage.setItem("heure");
-//     if(inpU == userN && inpP == userP &&  inpM == userM && inpG == userG && inpD == userD && inpH == userH){
-//        Window.Location.assign("NAN_GESTION_PROJET/index.html")
-//     }
-//     else{
-//        document.getElementById('error').style.display='block';
-//        document.getElementById('error').innerHTML="Incorrect Incorrect";
-//     }
-//    }
-
-//    let send = document.querySelector('#valider');
-//    send.addEventListener('click',(e) =>{
-//        e.preventDefault()
-//        let nom = document.querySelector('#nom').value;
-//        let prenom = document.querySelector('#prenom').value;
-//        let email = document.querySelector('#email').value;
-//        let genre = document.querySelector('#genre').value;
-//        let date = document.querySelector('#date').value;
-//        let heure = document.querySelector('#heure')
-//        let use = JSON.parse(localStorage.getItem("nom"));
-//        let prnam = JSON.parse(localStorage.getItem("prenom"));
-//        let mail = JSON.parse(localStorage.getItem("email"));
-//        let sexe = JSON.parse(localStorage.getItem("genre"));
-//        let jour = JSON.parse(localStorage.getItem("date"));
-//        let time = JSON.parse(localStorage.getItem("heure"));
-       
-//        console.log(use);
-//        if ((nom === localStorage.getItem("nom")) && prenom === localStorage.getItem("prenom") &&
-//         email === localStorage.getItem("email") && genre === localStorage.getItem("genre") 
-//         && date === localStorage.getItem("date") && heure  === localStorage.getItem("heure")){
-//            document.querySelector('#nom').value = JSON.parse(localStorage.getItem('user'));
-//            document.querySelector('#prenom').value = JSON.parse(localStorage.getItem('pass'));
-//            document.querySelector('#email').value = JSON.parse(localStorage.getItem('user'));
-//            document.querySelector('#genre').value = JSON.parse(localStorage.getItem('pass'));
-//            document.querySelector('#date').value = JSON.parse(localStorage.getItem('user'));
-//            document.querySelector('#time').value = JSON.parse(localStorage.getItem('pass'));
-//        }
-//        else{
-//            document.location.href="../HTML/dashboard.html";
-//        }
-//    });
+    console.log(btn);
